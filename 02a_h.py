e1da4dbf2be72bfd7c4dd78aef8a312b79379897
swk615 <107738125+swk615@users.noreply.github.com>
2022-06-18 11:42:33 +0900

    Add files via upload

---
+++ 02a_h.py
@@ -0,0 +1,9 @@
+#02a_h.py
+#인사말 출력하기
+
+print('hello')
+print('hello')
+print('hello')
+
+print('hello')
+
---
+++ 02b_tt.py
@@ -0,0 +1,27 @@
+#02b_tt.py
+#도형 그리기
+
+import turtle as t
+
+t.shape('turtle')
+
+#삼각형 그리기
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+
+#사각형 그리기
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(100)
+t.left(90)
+
+#원 그리기
+t.circle(50)
